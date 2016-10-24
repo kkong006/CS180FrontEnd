e21c5d438f51d36493c5c0a826c053a44a745deb
Karen Kong <kkong006@ucr.edu>
2016-10-24 12:20:56 -0700

    Add shared preferences to Constants class

--- app/src/main/java/teamawesome/cs180frontend/Misc/Constants.java
+++ app/src/main/java/teamawesome/cs180frontend/Misc/Constants.java
@@ -11,4 +11,9 @@ public class Constants {
     public static final int HTTP_STATUS_DNE = 401;
     public static final int HTTP_STATUS_ERROR = 500;
 
+    //Shared Preferences
+    public static final String USER_ID = "USER_ID";
+    public static final String PASSWORD_ID = "PASSWORD_ID";
+    public static final String SCHOOL_ID = "SCHOOL_ID";
+
 }
