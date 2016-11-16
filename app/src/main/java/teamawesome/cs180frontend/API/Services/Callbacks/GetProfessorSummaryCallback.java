6a94eae21fa0e5d20933b282475aea175a884cea
Karen Kong <kkong006@ucr.edu>
2016-11-16 11:58:21 -0800

    Finish professor stats page - Milestone 1 complete

--- app/src/main/java/teamawesome/cs180frontend/API/Services/Callbacks/GetProfessorSummaryCallback.java
+++ app/src/main/java/teamawesome/cs180frontend/API/Services/Callbacks/GetProfessorSummaryCallback.java
@@ -1,9 +1,13 @@
 package teamawesome.cs180frontend.API.Services.Callbacks;
 
+import org.greenrobot.eventbus.EventBus;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import teamawesome.cs180frontend.API.APIConstants;
 import teamawesome.cs180frontend.API.Models.ProfessorRespBundle;
+import teamawesome.cs180frontend.Misc.Utils;
 
 /**
  * Created by KongK on 11/15/2016.
@@ -13,11 +17,26 @@ public class GetProfessorSummaryCallback implements Callback<ProfessorRespBundle
     @Override
     public void onResponse(Call<ProfessorRespBundle> call, Response<ProfessorRespBundle> response) {
         System.out.println("PROFESSOR SUMMARY RESPONSE " + response.code());
+        switch (response.code()) {
+            case APIConstants.HTTP_STATUS_OK:
+                EventBus.getDefault().post(response.body());
+                break;
+            case APIConstants.HTTP_STATUS_INVALID:
+                EventBus.getDefault().post(0);
+                break;
+            case APIConstants.HTTP_STATUS_ERROR:
+                EventBus.getDefault().post(-1);
+                break;
+            default:
+                EventBus.getDefault().post(-1);
+                break;
+        }
 
     }
 
     @Override
     public void onFailure(Call<ProfessorRespBundle> call, Throwable t) {
-
+        System.out.println(Utils.getStackTrace(t));
+        EventBus.getDefault().post("ERROR");
     }
 }
